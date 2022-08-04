3dbdbee49c87afc16d5ed32e5c6c7f3eee8b2ae6
jiangxianbo <jxb42359@163.com>
2022-08-05 03:59:58 +0800

    api 参数

---
+++ go_5/tt.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	// 1.创建一个默认的路由引擎
+	// 默认使用了2个中间件 Logger(), Recovery()
+	r := gin.Default()
+
+	//// 也可以创建不带中间件的路由
+	//// r := gin.New()
+	//// 2.绑定路由，执行函数
+	//r.GET("/", func(c *gin.Context) {
+	//	c.String(http.StatusOK, "hello world!")
+	//	// c.JSON：返回JSON格式的数据
+	//	// c.JSON(200, gin.H{
+	//	//	 "message": "Hello world!",
+	//	// })
+	//})
+	//r.POST("/xxxPost", getting)
+
+	// api参数
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		c.String(http.StatusOK, name+" is "+action)
+	})
+
+	// 3.监听端口，默认8080
+	r.Run(":8000")
+}
+
+func getting(c *gin.Context) {
+
+}
